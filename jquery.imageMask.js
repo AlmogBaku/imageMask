374c31e769236c36e9b8a51073544caf438252aa
AlmogBaku <almog.baku@gmail.com>
2012-10-20 20:33:30 +0200

    fixing eviromental vars

--- jquery.imageMask.js
+++ jquery.imageMask.js
@@ -9,8 +9,6 @@
 
 (function($) {
   var $_count_id  = 0;
-  var $canvasObj  = null;
-  var $maskData   = null;
   
   $.fn.imageMask = function(_mask) {
     if(_mask==undefined) return false;
@@ -27,24 +25,24 @@
     var obj=this;
     
     $(maskObj).load(function() {
-      $maskData   = null;
+      var $maskData   = null;
       obj.each(function() {
         //reset      
-        $canvasObj  = null;
+        var $canvasObj  = null;
         
         //Create canvas
         $canvasObj = createCanvas(this, maskObj)[0];
         var ctx = $canvasObj.getContext("2d");
         
-        if($maskData==null) get_maskData(ctx, maskObj); //get mask data if not exsits
+        if($maskData==null) $maskData = get_maskData($canvasObj, ctx, maskObj); //get mask data if not exsits
         
         //reRender image
         var img = new Image();
         img.src = $(this).attr('src');
         $(img).load(function() {
-          drawImg(ctx, img);
+          drawImg($canvasObj, ctx, img);
           //Applying mask
-          applyMask(ctx);
+          applyMask($canvasObj, ctx, $maskData);
           
           $(obj).remove();
         });
@@ -74,25 +72,27 @@
        }).insertAfter(img);
   }
   
-  function get_maskData(ctx, mask) {
+  function get_maskData(canvasObj, ctx, mask) {
     ctx.drawImage(mask, 0, 0);                                                //draw image mask
-    $maskData = ctx.getImageData(0, 0, $canvasObj.width, $canvasObj.height); //save mask data
-    ctx.clearRect(0, 0, $canvasObj.width, $canvasObj.height);               //clear
+    var maskData = ctx.getImageData(0, 0, canvasObj.width, canvasObj.height); //save mask data
+    ctx.clearRect(0, 0, canvasObj.width, canvasObj.height);                   //clear
+    
+    return maskData;
   }
   
-  function drawImg(ctx, img) {
+  function drawImg(canvasObj, ctx, img) {
     //calculate ratio for scaling
     var ratio = 1;
-    if(img.width>img.height)  ratio = $canvasObj.height/img.height;
-    else                      ratio = $canvasObj.width/img.width;
+    if(img.width>img.height)  ratio = canvasObj.height/img.height;
+    else                      ratio = canvasObj.width/img.width;
     
     ctx.drawImage(img, 0,0, img.width, img.height, 0,0, img.width*ratio, img.height*ratio); //draw image based on ratio for resizing
   }
   
-  function applyMask(ctx) {
-    var imgData = ctx.getImageData(0, 0, $canvasObj.width, $canvasObj.height); //getting the image data
+  function applyMask(canvasObj, ctx, maskData) {
+    var imgData = ctx.getImageData(0, 0, canvasObj.width, canvasObj.height); //getting the image data
     for (var i=0; i<imgData.data.length; i+=4) {
-      imgData.data[i+3] = $maskData.data[i+3]; //replacing the point's alpha with the mask alpha
+      imgData.data[i+3] = maskData.data[i+3]; //replacing the point's alpha with the mask alpha
     }
     ctx.putImageData(imgData, 0, 0); //apply the changes
   }
